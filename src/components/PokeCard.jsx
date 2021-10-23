65f9145663db8ce89eaed560b2e0681a9183dd85
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-10-23 14:26:13 -0500

    finalizando pokeGrid

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function PokeCard() {
+export default function PokeCard(props) {
+
+    const URL = props.url;
+    const [pokemon, setPokemon] = useState(null)
+
+    useEffect(() => {
+
+        fetch(URL)
+            .then(result => result.json())
+            .then(data => setPokemon(data))
+
+    }, [URL]);
+
     return (<>
-            
-        PokeCards
 
-        </>)
+        <li className="card">
+            {pokemon ?
+                <img width={200} height={200} className="imagen" src={pokemon.sprites.other.dream_world.front_default} alt={props.nombre} />
+                :
+                <h4>cargando..</h4>
+            }
+            <div className="titulo">
+                <h5>{props.nombre}</h5>
+            </div>
+        </li>
+
+    </>)
 }
--- src/components/PokeGrid.jsx
+++ src/components/PokeGrid.jsx
@@ -1,9 +1,87 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import PokeCard from './PokeCard'
+
+const StyledContainer = styled.div`
+
+    .imagenGrid{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, 200px);
+        gap: 40px;
+        padding: 40px;
+        justify-content: center;
+    }
+`
+
+const StyledPagination = styled.div`
+    display: flex;
+    justify-content: center;
+
+`
 
 export default function PokeGrid() {
-    return (<>
-            
-        GridPoke
 
-        </>)
+    const [pokemones, setPokemones] = useState([])
+    const [page, setPage] = useState(1)
+    const [offSet, setOffSet] = useState(0)
+
+    useEffect(() => {
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offSet}&limit=25`)
+            .then(result => result.json())
+            .then(data => setPokemones(data))
+
+    }, [offSet]);
+
+    const handlePrevPage = (e) => {
+        e.stopPropagation()
+        setOffSet(offSet - 25)
+        setPage(page - 1)
+
+    }
+
+    const handleNextPage = async (e) => {
+        e.stopPropagation()
+        setOffSet(offSet + 25)
+        setPage(page + 1)
+
+
+    }
+
+    return (<StyledContainer>
+
+        <StyledPagination className="mt-5">
+            {page >= 2 &&
+                <button className="btn btn-outline-secondary m-3" onClick={handlePrevPage}>◀</button>
+            }
+
+            <h3 className="text-dark m-3">{page}</h3>
+
+            <button className="btn btn-outline-secondary m-3" onClick={handleNextPage}>▶</button>
+        </StyledPagination>
+
+        <ul className="imagenGrid">
+
+            {pokemones.results ?
+                pokemones.results.map((data, index) =>
+                    <PokeCard key={index} url={data.url} nombre={data.name} />
+                )
+                :
+                <h3>Cargando...</h3>
+            }
+        </ul>
+
+        <StyledPagination className="mt-1 mb-3">
+            {page >= 2 &&
+                <button className="btn btn-outline-secondary m-3" onClick={handlePrevPage}>◀</button>
+            }
+
+            <h3 className="text-dark m-3">{page}</h3>
+
+            <button className="btn btn-outline-secondary m-3" onClick={handleNextPage}>▶</button>
+        </StyledPagination>
+
+    </StyledContainer>)
 }
+
